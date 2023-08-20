1e37c30c5d1b5a9021a8fc755c88a1df6e0f8652
Matic Kerec <matic.kerec@gmail.com>
2023-08-20 17:26:09 +0200

    Updated-files

---
+++ build/precache-manifest.692e37ccc97e39a12936bdacd5ae1ba5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "16b0b4afa17ab9d84286",
+    "url": "/static/css/main.febf3626.chunk.css"
+  },
+  {
+    "revision": "16b0b4afa17ab9d84286",
+    "url": "/static/js/main.16b0b4af.chunk.js"
+  },
+  {
+    "revision": "f9a280cce4a84bd8f31a",
+    "url": "/static/js/1.f9a280cc.chunk.js"
+  },
+  {
+    "revision": "3a6c07f8cb8057b6a655",
+    "url": "/static/js/2.3a6c07f8.chunk.js"
+  },
+  {
+    "revision": "148dedf133217d189ab6",
+    "url": "/static/js/runtime~main.148dedf1.js"
+  },
+  {
+    "revision": "2f9fe6d1581bb19a5025f04318546b94",
+    "url": "/static/media/logoText_macka.2f9fe6d1.svg"
+  },
+  {
+    "revision": "f811fca18bdd8e1e0007ec257bcf8ce5",
+    "url": "/static/media/coffee_cup.f811fca1.jpg"
+  },
+  {
+    "revision": "474c69811aaf498812f01b1c1ffdb0ed",
+    "url": "/static/media/seasonal.474c6981.png"
+  },
+  {
+    "revision": "7719e95bc47b4d451c337f5553b6ff0c",
+    "url": "/static/media/lemonade.7719e95b.png"
+  },
+  {
+    "revision": "5112c65adab2ec6b14b8a7b1013e33da",
+    "url": "/static/media/croissant.5112c65a.png"
+  },
+  {
+    "revision": "3cebfef6f0cb69df8dc7a17615d93919",
+    "url": "/static/media/macka-at-night.3cebfef6.jpg"
+  },
+  {
+    "revision": "5bd35f5afc102aebff557640db2c398a",
+    "url": "/static/media/macka-notranjost.5bd35f5a.jpg"
+  },
+  {
+    "revision": "bcb0529aa08cd0774f53613859331640",
+    "url": "/static/media/izbira-pijac.bcb0529a.jpg"
+  },
+  {
+    "revision": "c05567de433d7c2e4ea8027d7b6bb74b",
+    "url": "/static/media/macka-text-below.c05567de.svg"
+  },
+  {
+    "revision": "8a2a541d4e80813ace9679637fdfef93",
+    "url": "/static/media/logoText_macka-white.8a2a541d.svg"
+  },
+  {
+    "revision": "36fdc600e998d9dcadba132431a1e985",
+    "url": "/static/media/white-texture.36fdc600.jpg"
+  },
+  {
+    "revision": "4a9b15dc1cfca11df49df1f8da3c1445",
+    "url": "/index.html"
+  }
+];
